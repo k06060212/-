26625d31034f865d99e0a1b85dc1ae8e58fb9590
k06060212 <k06060212@gmail.com>
2021-08-07 03:59:44 +0900

    동적 쿼리 처리를 위한 Querydsl 설정

---
+++ guestbook/src/main/java/org/zeroock/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,9 @@
+package org.zeroock.guestbook.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.zeroock.guestbook.entity.Guestbook;
+
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
+}
